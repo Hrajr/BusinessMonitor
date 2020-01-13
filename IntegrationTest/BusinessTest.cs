8d2e5c7b3a60f783c66a3db11cdef929f6945199
Hrajr Hakobjan <424119@student.fontys.nl>
2020-01-13 21:06:42 +0100

    Basic intergration test werkt.. nu alleen paar unit testen om aan te tonen dat ik het begrijp

--- IntegrationTest/BusinessTest.cs
+++ IntegrationTest/BusinessTest.cs
@@ -19,12 +19,14 @@ namespace IntegrationTest
 
         [Theory]
         [InlineData("/")]
-        [InlineData("/EditInvoice")]
         [InlineData("/Invoice")]
-        [InlineData("/EditItem")]
         [InlineData("/Item")]
-        [InlineData("/EditReference")]
         [InlineData("/Reference")]
+        [InlineData("/Registration")]
+        [InlineData("/Login")]
+        [InlineData("/EditInvoice?id=Purchase%3A01%3A13%3A2020%3A20%3A37%3A57")]
+        [InlineData("/EditItem?id=5C704287-C86E-4FC6-B307-44128CCEF36C")]
+        [InlineData("/EditReference?id=5069609E-5383-4D86-8CFE-A7B8BFC38C38")]
         public async Task HttpRequest(string url)
         {
             // Arrange
